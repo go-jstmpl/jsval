6e09a4d55e4edb3cdf113809ceaec31a5cf66415
minodisk <daisuke.mino@gmail.com>
2016-10-18 19:42:46 +0900

    Add test for the interface having length property

--- test/max_items_test.ts
+++ test/max_items_test.ts
@@ -55,6 +55,69 @@ describe("MaxItemsValidator", () => {
       });
     });
 
+    it(`should validate object which has length property`, () => {
+      class HasLengthProperty {
+        public length: number;
+        constructor(private items: any[]) {
+          this.length = items.length;
+        }
+      }
+
+      class HasLengthGetter {
+        public get length(): number { return this.items.length; }
+        constructor(private items: any[]) {}
+      }
+
+      const definition = {
+        maxItems: 3,
+      };
+      const validator = new MaxItemsValidator(definition);
+      [
+        {
+          input: {
+            length: 3,
+          },
+          expected: undefined,
+        },
+        {
+          input: {
+            length: 4,
+          },
+          expected: {
+            input: {
+              length: 4,
+            },
+            definition,
+          },
+        },
+        {
+          input: new HasLengthProperty([1, 2, 3]),
+          expected: undefined,
+        },
+        {
+          input: new HasLengthProperty([1, 2, 3, 4]),
+          expected: {
+            input: new HasLengthProperty([1, 2, 3, 4]),
+            definition,
+          },
+        },
+        {
+          input: new HasLengthGetter([1, 2, 3]),
+          expected: undefined,
+        },
+        {
+          input: new HasLengthGetter([1, 2, 3, 4]),
+          expected: {
+            input: new HasLengthGetter([1, 2, 3, 4]),
+            definition,
+          },
+        },
+      ].forEach(({input, expected}) => {
+        const actual = validator.validate(input);
+        assert.deepEqual(actual, expected);
+      });
+    });
+
   });
 
 });
