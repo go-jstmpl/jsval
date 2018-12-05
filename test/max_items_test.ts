6a63986be64f80dfb076401620d1507a988acbd9
PyYoshi <myoshi321go@gmail.com>
2018-12-05 12:41:01 +0900

    remove unnecessary tests

--- test/max_items_test.ts
+++ test/max_items_test.ts
@@ -81,69 +81,6 @@ describe("MaxItemsValidator", () => {
       });
     });
 
-    it(`should validate object which has length property`, () => {
-      class HasLengthProperty {
-        public length: number;
-        constructor(private items: any[]) {
-          this.length = items.length;
-        }
-      }
-
-      class HasLengthGetter {
-        public get length(): number { return this.items.length; }
-        constructor(private items: any[]) {}
-      }
-
-      const definition = {
-        maxItems: 3,
-      };
-      const validator = new MaxItemsValidator(definition);
-      [
-        {
-          input: {
-            length: 3,
-          },
-          expected: null,
-        },
-        {
-          input: {
-            length: 4,
-          },
-          expected: {
-            input: {
-              length: 4,
-            },
-            definition,
-          },
-        },
-        {
-          input: new HasLengthProperty([1, 2, 3]),
-          expected: null,
-        },
-        {
-          input: new HasLengthProperty([1, 2, 3, 4]),
-          expected: {
-            input: new HasLengthProperty([1, 2, 3, 4]),
-            definition,
-          },
-        },
-        {
-          input: new HasLengthGetter([1, 2, 3]),
-          expected: null,
-        },
-        {
-          input: new HasLengthGetter([1, 2, 3, 4]),
-          expected: {
-            input: new HasLengthGetter([1, 2, 3, 4]),
-            definition,
-          },
-        },
-      ].forEach(({input, expected}) => {
-        const actual = validator.validate(input);
-        assert.deepEqual(actual, expected);
-      });
-    });
-
   });
 
 });
